99ecf66a7fa1f0bc753779e01d6d52461cf241fd
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-01-02 20:10:00 +0100

    starting point

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './partials/navbar/navbar/navbar.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { SidebarComponent } from './partials/sidebar/sidebar.component';
+import { DefaultComponent } from './layout/default/default.component';
+import { BlankComponent } from './layout/blank/blank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    DefaultComponent,
+    BlankComponent
   ],
   imports: [
     BrowserModule,
