196477c1dc667a78d48f58d901b070bba2d127c3
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-01-02 21:36:23 +0100

    setup layout and route for application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+// bing in layout
+import { DefaultComponent } from "./layout/default/default.component";
+import { BlankComponent } from "./layout/blank/blank.component";
+// layout ends here
 
+// bring in components
+import { ProductsComponent } from "./components/products/products.component";
+import { UsersComponent } from "./components/users/users.component";
+import { LoginComponent } from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: BlankComponent,
+    children: [{ path: "", component: LoginComponent }]
+  },
+  {
+    path: "",
+    component: DefaultComponent,
+    children: [
+      { path: "products", component: ProductsComponent },
+      { path: "users", component: UsersComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
