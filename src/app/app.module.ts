c15cd0071bd578aa1bae300b8cebb13a8a958a75
Amanam Israel <you@example.com>
2020-01-09 23:09:40 +0100

    added charts to home.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SubnavComponent } from "./partials/subnav/subnav.component";
 import { LoginComponent } from './components/login/login.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { HomeComponent } from './components/home/home.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -28,9 +29,11 @@ import { HomeComponent } from './components/home/home.component';
     SubnavComponent,
     LoginComponent,
     AddProductComponent,
-    HomeComponent
+    HomeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule,
+     AppRoutingModule,
+    ChartsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    barChartOptions: ChartOptions = {
+        responsive: true,
+      };
+      barChartLabels: Label[] = ['Apple', 'Samsung', 'Lenovo', 'Huawei', 'Oppo', 'Xiaomi'];
+      barChartType: ChartType = 'bar';
+      barChartLegend = true;
+      barChartPlugins = [];
 
-  constructor() { }
+      barChartData: ChartDataSets[] = [
+        { data: [45, 37, 60, 70, 46, 50], label: 'Best Gadgets' }
+      ];
 
+      doughnutChartLabels: Label[] = ['Beans', 'Air pods', 'Chargers'];
+        doughnutChartData: MultiDataSet = [
+            [55, 25, 20]
+        ];
+        doughnutChartType: ChartType = 'doughnut';
+
+
+  constructor() {}
   ngOnInit() {
   }
+
 
 }
